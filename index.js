e620acd6df1f6dbffc80395c3d41a887c8e1563a
raidislam <raidislam@gmail.com>
2025-02-03 12:50:56 +0600

    server

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const port = process.env.PORT || 5050
+
+// Middleware
+
+app.use(cors())
+app.use(express.json())
+
+// Starting URL
+
+app.get("/", (req, res) => {
+  res.send("Job Portal")
+})
